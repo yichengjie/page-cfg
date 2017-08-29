62199a7bbf1238ab65d331b6c0108cf31b567100
yichengjie <626659321@qq.com>
2017-08-29 14:02:25 +0800

    update

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -6,6 +6,6 @@ export interface HelloProps { compiler: string; framework: string; }
 // State is never set so we use the 'undefined' type.
 export class Hello extends React.Component<HelloProps, undefined> {
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return <h1>Hello from  {this.props.compiler} and {this.props.framework}!</h1>;
     }
 }
