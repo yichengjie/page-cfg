8599c72dc0c1ac551e96dda6952befb1d9b44fdd
yichengjie <626659321@qq.com>
2017-08-30 12:42:35 +0800

    BaseTable组件开发，暂时保存

--- table1/src/components/BaseTable.tsx
+++ table1/src/components/BaseTable.tsx
@@ -1,34 +1,115 @@
 import * as React from "react";
 
+interface TableColumnDescr{
+    title:string ;
+    dataItemKey:string ;
+    width?:number ;
+    render?:string ;
+}
+
+export interface TableSchemaDescr{
+    columns:TableColumnDescr[] ;
+    rowKeyNames:string [] ;
+}
+
 export interface BaseTableProps {
-    compiler: string; framework: string; 
+    list: object[]; 
+    schema: TableSchemaDescr; 
+    description:string ; /**表格的描述信息，暂定后期可能会使用*/
+    code:string ;/**表格code ，暂定后期可能会使用 */
 }
 
 class  BaseTable extends React.Component<BaseTableProps,any>{
     constructor(props:BaseTableProps){
         super(props) ;
     }
+    /**显示表头信息 */
+    renderTheader(columns:TableColumnDescr[]) {
+        return (
+            <tr>
+                {
+                    columns.map(function(item,index){
+                        let {width} = item ;
+                        let tmpObj = {width} ;
+                        return<th {...tmpObj} key={item.dataItemKey} >{item.title}</th> ;
+                    }) 
+                }
+            </tr>
+        ) ;
+    }
+
+    /**
+     * 显示表格的每一行
+     */
+    renderRowItem(rowItem:{[index:string]:any} ,schema:TableSchemaDescr){
+        let {columns,rowKeyNames} = schema ;
+        let rowKeyValues = rowKeyNames.map(function(rowKeyName){
+            return (rowItem[rowKeyName] || 'null') ; 
+        }) ;
+        let rowKeyStr = rowKeyValues.join('_') ; ;
+        return (
+            <tr key={rowKeyStr}>
+                {
+                    columns.map((column,index) => {
+                        let {dataItemKey,render} = column ;
+                        let value = rowItem[dataItemKey] || '' ;
+                        return (<td key={index} >{this.renderTdContent(value,render)}</td>) ;
+                    }) 
+                }
+            </tr>
+        ) ;
+    }
+
+
+
+    exeRenderTdFuncByName(value:string,funcName:string){
+        if(funcName && (typeof funcName === 'string')){
+            let fn = this[funcName] as function; 
+            return fn(value) ;
+        }
+        return value ;
+    }
+
+
+    /**
+     * 
+     * @param value 
+     * @param render 
+     *  dealColumns(columns){
+            let newColumns = [] ;
+            for(let column of columns){
+                let {render,...other} = column ;
+                let obj = {...other} ;
+                if(render != null){
+                    obj.render = this[render] ;
+                }
+                newColumns.push(obj) ;
+            }
+            return newColumns ;
+        }
+     */
+
+    renderTdContent(value:string,render:string){
+       
+        return this.exeRenderTdFuncByName(value,render) ;
+    }
+
+    renderTbody(list:object[] ,schema:TableSchemaDescr){
+        return list.map((item) => {
+            return this.renderRowItem(item,schema) ;
+        }) ;
+    }
+
     render(){
+      let {schema , list , description , code} = this.props ;
+      let {columns,rowKeyNames} = schema ;
       return (
-          <table>
+          <table className="table table-bordered">
               <thead>
-                  <tr>
-                      <th>用户名</th>
-                      <th>电话</th>  
-                      <th>邮箱</th>          
-                  </tr>
+                {this.renderTheader(columns)}  
               </thead>
               <tbody>
-                  <tr>
-                      <td>yicj</td>
-                      <td>133</td>
-                      <td>62@qq.com</td>    
-                  </tr>
-                  <tr>
-                      <td>{this.props.compiler}</td>
-                      <td>{this.props.framework}</td>
-                      <td>62@qq.com</td>    
-                  </tr>
+                  {this.renderTbody(list,schema)}
               </tbody>
           </table>
       ) ;  
--- table1/src/components/Hello.tsx
+++ table1/src/components/Hello.tsx
@@ -1,30 +1,78 @@
 import * as React from "react";
-import BaseTable,{BaseTableProps} from './BaseTable' ;
+import BaseTable,{BaseTableProps,TableSchemaDescr} from './BaseTable' ;
 
-export interface HelloProps { compiler: string; framework: string; }
+const tableSchema ={
+    columns:[//-------------定义每一列如何显示
+        { 
+          title: '用户名', dataItemKey: 'username', width: 100,
+          render:'showUserNameColumnFnName',//用户可能需要显示高亮一点
+        },
+        { title: '专业', dataItemKey: 'dept',  width: 100 },
+        { title: '主页', dataItemKey: 'homepage',width: 200,
+           dataItemColumnRender:'showMyHomePageFnName'//主页可能需要显示成超链接
+        },
+        //{ title: '操作', dataItemKey: '',  width:300 },//dateKey为空，标识为操作列
+    ],
+    rowKeyNames:['username','dept'] //--定义每一行记录根据哪几个字段能确定数据库中的一条记录
+}
 
-// // 'HelloProps' describes the shape of props.
-// // State is never set so we use the 'undefined' type.
-// export class Hello extends React.Component<HelloProps, undefined> {
-    
-//     render() {
-//         return <h1>Hello from  {this.props.compiler} and {this.props.framework}!</h1>;
-//     }
-// }
+const data = [
+    { username: '张三',dept:"软件工程",homepage:'wwww.baidu.com' ,key:'001'}, 
+    //key 为查询记录不重复的一个字符串，可以不传，如果不传会根据tableSchema中定义的主键字段自动拼接字符串
+    { username: '李四',dept:"英语",homepage:'www.jd.com' ,key:'002' }
+];
 
 //自定义表格本页面的表格显示
 class MyTable extends BaseTable{
 
 }
 
+interface HelloStates{
+    list:object[] ;
+    schema:TableSchemaDescr ;
+}
+
 //查询页面代码
-export class Hello extends React.Component<HelloProps, undefined> {
+export class Hello extends React.Component<any, HelloStates> {
+    constructor(){
+        super() ;
+        this.state = {
+            list:[],
+            schema:{
+                columns:[],
+                rowKeyNames:[]
+            } 
+        } ;
+    }
+
+
+    componentDidMount(){
+        //先去查询表格显示的schema
+        this.setState({
+            schema:tableSchema
+        }) ;
+
+        setTimeout(()=>{
+            this.setState({list:data}) ;
+        },20) ;
+
+    }
+
+    showMyHomePageFnName(value:string){
+        return (
+            <a href="#">{value}</a>
+        ) ;
+    }
+
     render() {
-        let props = this.props ;
         return (
             <div>
-                <h1>Hello from  {this.props.compiler} and {this.props.framework}!</h1>
-                <MyTable {...props} />
+                <h1>Hello workd!</h1>
+                <MyTable  code ="table163" 
+                    description="大客户表信息描述" 
+                    list={this.state.list}
+                    schema={this.state.schema}
+                    />
             </div>
         ) ; 
     }
